8085c16b5360afe1224e56068d758bbd0a3767a9
Wang-Phil <1420767429@qq.com>
2024-03-23 13:25:56 +0800

    已经完成基础分类的代码

--- SimpleNetWork/BinaryClassificationNetWork.py
+++ SimpleNetWork/BinaryClassificationNetWork.py
@@ -3,13 +3,16 @@
 import matplotlib.pyplot as plt
 from sklearn.cluster import SpectralClustering
 import sklearn.datasets
+import numpy as np
 #搭建网络的包
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+#统计分数
+from sklearn.metrics import accuracy_score
 
-#生成200个点的数据集，返回的结果是一个包含两个元素的元组 (X, y),X是点，y是x的分类
-X, y =  sklearn.datasets.make_moons(200, noise=0.2)
+#生成300个点的数据集，返回的结果是一个包含两个元素的元组 (X, y),X是点，y是x的分类
+X, y =  sklearn.datasets.make_moons(300, noise=0.2)
 
 save_path = "/data/wangweicheng/ModelLearning/SimpleNetWork"
 plt.scatter(X[:,0],X[:,1],s=40,c=y,cmap=plt.cm.Spectral)
@@ -37,17 +40,19 @@ class BinaryClassifier(nn.Module):
         x = self.output(x)
         return x
     
-
-     #This function takes an input and predicts the class, (0 or 1)        
+      
     def predict(self,x):
-        #Apply softmax to output
-        pred = F.softmax(self.forward(x))
+        #训练好之后，重新将x输入到网络中，进行测试
+        #softmax得到0到1的一个值
+        pred = F.softmax(self.forward(x),dim=1)
         ans = []
+        #对不同点分别进行判断，如果第一个位置的数值大于第二个位置，就返回0，反之返回1
         for t in pred:
             if t[0]>t[1]:
                 ans.append(0)
             else:
                 ans.append(1)
+        return torch.tensor(ans)
 
 
 #初始化模型
@@ -59,7 +64,7 @@ loss_criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
 
 #训练的次数
-epochs = 10
+epochs = 5000
 #存储loss
 losses = []
 
@@ -76,7 +81,34 @@ for i in range(epochs):
     loss.backward()
     #梯度优化
     optimizer.step()
+    if(i % 500 == 0):
+        print('loss: {:.4f}'.format(loss.item()))
 
 
-from sklearn.metrics import accuracy_score
+#进行预测
 print(accuracy_score(model.predict(X),y))
+
+def predict(x):
+    x = torch.from_numpy(x).type(torch.FloatTensor)
+    ans = model.predict(x)
+    return ans.numpy()
+    
+# 画出边框
+def plot_decision_boundary(pred_func,X,y):
+    # 找到x，y的最大和最小值，并填充一些边框
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # 生成一个网格
+    xx,yy=np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # 将两个展平后的一维数组 xx.ravel() 和 yy.ravel() 按列连接起来，生成一个二维数组，然后对这个点进行预测
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    # 将预测结果 Z 重塑为与点网格 xx 相同的形状
+    Z = Z.reshape(xx.shape)
+    # 画出图像，绘制等高线图的代码
+    plt.contourf(xx, yy, Z, cmap=plt.cm.YlGnBu)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.binary)
+    plt.savefig(f'{save_path}/dataout.png')
+    
+    
+plot_decision_boundary(lambda x : predict(x) ,X.numpy(), y.numpy())
